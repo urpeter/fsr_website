99e2ad37441941c94a457a19195dad6719d6bda5
urs <36850433+urpeter@users.noreply.github.com>
2021-06-15 15:32:45 +0200

    changed signature

--- insa_push.sh
+++ insa_push.sh
@@ -1,1 +1,1 @@
-./gradlew build && rsync -avcp --chmod=o+rx,g+rwx,u+rwx build/site/ urspeter@httpd.coli.uni-saarland.de:/web/www.coli.uni-saarland.de/html/fs-coli/
+./gradlew build && rsync -avcp --chmod=o+rx,g+rwx,u+rwx build/site/ urspeter@httpd.coli.uni-saarland.de:/web/www.coli.uni-saarland.de/html/fs-lst/
